803e91eeb684dfcf3fe4cf72605c26cb657ac0de
Matthias Colin <matthias.colin@gmail.com>
2021-01-06 16:07:18 +0100

    add glaçage

--- patebrisee.js
+++ patebrisee.js
@@ -2,4 +2,5 @@ function recette() {
     console.log("Une bonne tarte");
     console.log("au citron c'est bon");
     console.log("avec de la meringue");
+    console.log("un glaçage à la confiture");
 }
